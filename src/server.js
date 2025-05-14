f5cc6be2dd0e95b9ad2808b1acd13d336df66ae9
Papant <andriy.chornenko@ukr.net>
2025-05-14 19:40:52 +0300

    markup

--- src/server.js
+++ src/server.js
@@ -1,34 +1,11 @@
 import express from 'express';
-import logger from 'morgan';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import morgan from 'morgan';
 import contactsRouter from './routes/contactRoutes.js';
-
-dotenv.config();
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
-
 app.use('/api/contacts', contactsRouter);
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
-});
-
-app.use((err, req, res) => {
-  res.status(500).json({ message: err.message });
-});
-
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log('Database connection successful'))
-  .catch((error) => {
-    console.error('Database connection failed');
-    process.exit(1);
-  });
 
 export default app;
