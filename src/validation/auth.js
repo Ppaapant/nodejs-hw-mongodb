cee7e408dfa65ab44bbba0b1c0c6db83535dcd2d
Papant <andriy.chornenko@ukr.net>
2025-06-08 15:42:16 +0300

    valUPD

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,12 +1,12 @@
 import Joi from 'joi';
 
 export const registerSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required()
+  name: Joi.string(),
+  email: Joi.string().email(),
+  password: Joi.string().min(6)
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required()
+  email: Joi.string().email(),
+  password: Joi.string().min(6)
 });
